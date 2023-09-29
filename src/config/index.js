43506371a0aff7c46ce4afaafa19a1713d236453
emmanuel <sandoval.morales.emmanuel@gmail.com>
2023-09-29 12:24:27 -0600

    update url change

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 
 const config = {
-    base_URL: 'https://api-locallity.up.railway.app/api/v1',
+    base_URL: 'https://api-business-registration-production.up.railway.app/api/v1',
     selectAll: '/selectAll',
     locality: '/locality',
     filter: '/filter',
